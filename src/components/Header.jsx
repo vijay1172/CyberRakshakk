ef8a45289f35e7c43484cbdef2b027b3a98823b8
student-Swati <ruchiswati1631@gmail.com>
2024-07-06 12:33:51 +0530

    loginpage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import './Header.css'
+import './Header.css';
 
 function Header() {
   return (
     <header>
-      <nav >
+      <nav className='navigation-bar'>
         <ul className='header-container'>
           <li><a href="#hero">Home</a></li>
           <li><a href="#quiz">Quiz</a></li>
           <li><a href="#articles">Articles</a></li>
           <li><a href="#safety-tips">Safety Tips</a></li>
           <li><a href="#resources">Resources</a></li>
+          <a  className = "login-btn" href="#login/signup">Login</a>
         </ul>
       </nav>
     </header>
