bf0a5b456cb4047f1352b03fb2f2ef89158c96e7
student-Swati <ruchiswati1631@gmail.com>
2024-07-07 11:12:56 +0530

    herosection changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import './HeroSection.css'
+import './HeroSection.css';
 
 function HeroSection() {
   return (
-    
-    <section className="hero-section">
-      <h1 className='heading'>Welcome to CyberRakshak</h1>
-      <p>Learn, test your knowledge, and stay safe online</p>
+    <section className="hero-container">
+      <div className="hero-content">
+        <h1 className='heading'>Welcome to CyberRakshak</h1>
+        <p>Learn, test your knowledge, and stay safe online</p>
+      </div>
     </section>
   );
 }
 
-export default HeroSection; 
+export default HeroSection;
