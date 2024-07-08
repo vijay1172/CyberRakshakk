ed0e66f09af94fe1ad7a798782e2d9ec2ab0b12e
student-Swati <ruchiswati1631@gmail.com>
2024-07-08 18:30:57 +0530

    created login page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import logo from '../assets/logo.png';
 
 function Header() {
-  const scrollToSection = (sectionId) => {
+  const scrollToSection = (sectionId, event) => {
+    event.preventDefault(); // Prevent default link behavior
     const element = document.getElementById(sectionId);
     if (element) {
-      // Prevent default link behavior (optional but recommended)
-      event.preventDefault(); // Added line
-
       element.scrollIntoView({ behavior: 'smooth' });
     }
   };
 
   return (
     <header>
-      <nav className='navigation-bar'>
-        <ul className='header-container'>
-          <li><Link to="/" onClick={(event) => scrollToSection('hero')}>Home</Link></li>
-          <li><Link to="/" onClick={(event) => scrollToSection('quiz')}>Quiz</Link></li>
-          <li><Link to="/" onClick={(event) => scrollToSection('articles')}>Articles</Link></li>
-          <li><Link to="/" onClick={(event) => scrollToSection('safety-tips')}>Safety Tips</Link></li>
-          <li><Link to="/" onClick={(event) => scrollToSection('resources')}>Resources</Link></li>
-          <li><Link className="login-btn" to="/login">Login</Link></li>
+      <div className="logo">
+        <Link to="/"><img src={logo} alt="Logo" /></Link>
+      </div>
+      <nav>
+        <ul className="header-container">
+          <li><Link to="/" onClick={(event) => scrollToSection('hero', event)}>Home</Link></li>
+          <li><Link to="/" onClick={(event) => scrollToSection('quiz', event)}>Quiz</Link></li>
+          <li><Link to="/" onClick={(event) => scrollToSection('articles', event)}>Articles</Link></li>
+          <li><Link to="/" onClick={(event) => scrollToSection('safety-tips', event)}>Safety Tips</Link></li>
+          <li><Link to="/" onClick={(event) => scrollToSection('resources', event)}>Resources</Link></li>
         </ul>
+        <Link className="login-btn" to="/login">Login</Link>
       </nav>
     </header>
   );
